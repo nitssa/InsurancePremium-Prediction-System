fa5e93b4ff68d81639d9dcff621ab5323b71e123
Nitsa <nitushapaudel10@gmail.com>
2025-02-12 10:35:55 +0545

    uploaded files

---
+++ Insurance/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from Insurance import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('prediction', views.prediction, name='prediction'),
+    path('login', views.login, name='login'),
+    path('registration', views.registration, name='registration'),
+]
